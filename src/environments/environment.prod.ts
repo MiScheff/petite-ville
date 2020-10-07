6353f01bfe3780d1dca97526a148d47005880a23
Michel <michel.scheff@gmail.com>
2020-10-07 23:20:01 +0200

    Set up the Firebase real-time database and the Firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC6cJrEVbSNCQCaO9P11anQ6aQhs7BAZbI',
+    authDomain: 'petite-ville.firebaseapp.com',
+    databaseURL: 'https://petite-ville.firebaseio.com',
+    projectId: 'petite-ville',
+    storageBucket: 'petite-ville.appspot.com',
+    messagingSenderId: '269613152962',
+    appId: '1:269613152962:web:7cc2d2bc845a44b0fc195b'
+  }
 };
