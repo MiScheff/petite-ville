541d1b3cf3efbf28c8fb18e80b58ddebaed5b874
Michel <michel.scheff@gmail.com>
2020-10-08 19:22:12 +0200

    Style roughly the home page

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,16 +8,22 @@ import { PartiesService } from 'src/app/services/parties.service';
   styleUrls: ['./menu.component.sass']
 })
 export class MenuComponent implements OnInit {
+  isLogged = false;
 
   constructor(private router: Router,
               private partiesS: PartiesService) { }
 
   ngOnInit(): void {
+    // TODO : Vérifier si l'utilisateur est connecté
   }
 
   async newGame() {
     const partieID = (await this.partiesS.createPartie()).key;
     this.router.navigate(['/partie/', partieID]);
+  }
+
+  toggle() {
+    this.isLogged = !this.isLogged;
   }
 
 }
